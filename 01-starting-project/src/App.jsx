df502290aa6b426af04a78769f2c13509167184d
vasuwebdeveloper <kasipurivasu@gmail.com>
2024-10-21 01:11:11 +0530

    seperation of components

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,41 +1,10 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  {
-    /*POC #2: dynamic content} */
-  }
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreComponent.jsx";
 {
   /*POC #3: understanding props} */
 }
-function CoreConcept({ title, description, image }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+
 
 function App() {
   return (
